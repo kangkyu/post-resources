0027bb8ed0c66d4f615e8dc7c4eb29775546525b
kangkyu <kangkyu1111@gmail.com>
2015-04-07 10:26:16 -0700

    add user edit update and show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, except: [:new, :create]
+  before_action :load_user, only: [:show, :edit, :update]
   def show
-    @user = User.find(params[:id])
   end
   def new
     @user = User.new
   end
   def create
-    @user = User.new(params.require(:user).permit!)
+    @user = User.new(params.require(:user).permit(:username, :password, :password_confirmation))
     if @user.save
       redirect_to root_url, notice: "notice. registered and logged in"
       session[:user_id] = @user.id
@@ -16,4 +16,27 @@ class UsersController < ApplicationController
       render action: 'new'
     end
   end
+  def edit
+    unless correct_user?
+      flash[:error] = "error. can edit only current user"
+      redirect_to user_url(@user)
+    end
+  end
+  def update
+    if correct_user? && @user.update(params.require(:user).permit(:username, :password, :password_confirmation))
+      redirect_to user_url(@user)
+    else
+      render action: 'edit'
+    end
+  end
+
+  private
+
+  def load_user
+    @user = User.find(params[:id])
+  end
+  def correct_user?
+    session[:user_id] == @user.id
+  end
+  helper_method :correct_user?
 end
