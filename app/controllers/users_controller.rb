b2370e9c361a978ce391a1b621881128196970f2
kangkyu <kangkyu1111@gmail.com>
2015-04-07 10:48:56 -0700

    list all posts and comments on user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user, except: [:new, :create]
+  before_action :authenticate_user, except: [:new, :create, :show]
   before_action :load_user, only: [:show, :edit, :update]
   def show
   end
