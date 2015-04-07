9d746a48ad06bbad1bc524ab4a0c2b055523a1cb
kangkyu <kangkyu1111@gmail.com>
2015-04-06 18:59:49 -0700

    add users_controller new action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(params.require(:user).permit!)
+    if @user.save
+      redirect_to root_url, notice: "registered and logged in"
+      session[:user_id] = @user.id
+    else
+      flash[:error] = "error. try again"
+      render 'new'
+    end
+  end
 end
