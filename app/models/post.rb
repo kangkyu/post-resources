bbf22914e934cd0b738a7bd7ec1113b52248c4a3
kangkyu <kangkyu1111@gmail.com>
2015-05-11 11:06:56 -0700

    add dependent destroy comments and join to categories, post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
-  has_many :comments
-  has_many :posts_categories
+  has_many :comments, dependent: :destroy
+  has_many :posts_categories, dependent: :destroy
   has_many :categories, through: :posts_categories
   belongs_to :user
 
