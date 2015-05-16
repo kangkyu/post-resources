4691d21d1e7b1a5776cc166bf66a250e7a646f6e
kangkyu <kangkyu1111@gmail.com>
2015-05-15 21:16:02 -0700

    additional style - titles, buttons, all categories block

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   before_action :authenticate_user
 
   def create
+    # fail
     @post = Post.find(params[:post_id])
     comment = @post.comments.build(params.require(:comment).permit(:body))
     comment.user_id = session[:user_id]
@@ -9,7 +10,7 @@ class CommentsController < ApplicationController
       redirect_to @post, notice: "notice. comment added"
     else
       flash[:error] = "error. comment not saved"
-      render template: 'posts/show'
+      redirect_to @post
     end
   end
 
