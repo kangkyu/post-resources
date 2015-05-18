9b9dfab41ece27783d69471095f7cb6808737fe7
kangkyu <kangkyu1111@gmail.com>
2015-05-18 15:03:25 -0700

    keep category names unique as lower case

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,6 +7,7 @@ class CategoriesController < ApplicationController
 
   def create
     @category = Category.new(params.require(:category).permit(:name))
+    @category.name.downcase!
     if @category.save
       redirect_to root_url, notice: "notice. category added"
     else
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ActiveRecord::Base
   has_many :posts, through: :posts_categories
   has_many :posts_categories
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
