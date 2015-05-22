f6938c167342870f4f0b1e8d9d15ef83bf5d91ba
kangkyu <kangkyu1111@gmail.com>
2015-05-22 13:45:07 -0700

    test login controller and login feature

---
+++ test/controllers/login_controller_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class LoginControllerTest < ActionController::TestCase
+
+  def test_get_new
+    get :new
+    assert_response :success
+  end
+
+  def test_login_create
+    user = users(:one)
+    post :create, login: { username: users(:one).username, password: "password" }
+    assert_equal user.id, session[:user_id]
+  end
+
+  def test_login_destroy
+    session[:user_id] = users(:one).id
+    delete :destroy
+    assert_equal nil, session[:user_id]
+  end
+end
--- test/features/login_user_test.rb
+++ test/features/login_user_test.rb
@@ -3,19 +3,32 @@ require "test_helper"
 class LoginUserTest < Capybara::Rails::TestCase
   test "before login" do
     visit root_path
-    assert_content page, "register"
-    assert_content page, "login"
+
+    assert_link "register"
+    assert_link "login"
   end
 
   test "after login" do
-    visit login_path
+    test_log_in
+    visit root_path
 
+    assert_link "profile"
+    assert_link "logout"
+  end
+
+  test "log out" do
+    test_log_in
+    click_link "logout"
+
+    assert_text "user logged out"
+  end
+
+  test "log in" do
+    visit login_path
     fill_in "Username", with: users(:one).username
     fill_in "Password", with: 'password'
     click_button "Login"
 
-    visit root_path
-    assert_content page, "profile"
-    assert_content page, "logout"
+    assert_text "user logged in"
   end
 end
