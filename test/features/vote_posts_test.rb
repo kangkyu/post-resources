3fa4ab06418846d81eee994d85ffc0e39689e724
Kang-Kyu Lee <kangkyu1111@gmail.com>
2019-12-07 22:09:07 -0800

    Test the case a user click vote without login

--- test/features/vote_posts_test.rb
+++ test/features/vote_posts_test.rb
@@ -22,10 +22,23 @@ class VotePostsTest < Capybara::Rails::TestCase
     assert_equal "-1", find(id: "post_#{post.id}_vote").text
   end
 
+  def test_downvote_without_login_show_page
+    log_out
+    post = posts(:two)
+    visit post_path(post)
+    find_link(class: 'vote-down-post').click
+
+    assert_content "error. login needed to vote"
+  end
+
   def log_in
     visit login_path
     fill_in "Username", with: users(:one).username
     fill_in "Password", with: 'password'
     click_button "Login"
+  end
+
+  def log_out
+    click_link "logout"
   end
 end
